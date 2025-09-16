64760b57329324ebf0266794b10bc6ce18ad132c
Rahmad Dwirizki Olders <rizkimords@gmail.com>
2025-09-16 11:42:56 +0700

    praktikum pekan 2

---
+++ 2025_prakalpro_C_2511532025/src/pekan2/ContohChar.java
@@ -0,0 +1,38 @@
+package pekan2;
+
+public class ContohChar {
+
+	public static void main(String[] args) {
+		// Deklarasi variabel char
+		char huruf1 = 'A';
+		char huruf2 = 'B';
+		char angka= '7';
+		char simbol = '#';
+		
+		// Menampilkan Nilai Variabel Char 
+		System.out.println("Contoh variabel char:");
+		System.out.println("Huruf pertama: " + huruf1);
+		System.out.println("Huruf kedua: "+ huruf2);
+		System.out.println("Angka: "+ angka);
+		System.out.println("Simbol: "+simbol);
+		
+		// Operasi Dengan Char (berdasarkan kode Unicode/ASCII)
+		char huruf3 = (char) (huruf1 + 1); // A (65) + 1 = B (66)
+		System.out.println("Huruf1 + = " + huruf3);
+		
+		// Char jiga bisa disimpan dalam Integer (ASCII/Unicode value)
+		int kodehuruf = huruf1;
+		String biner1 = String.format("%8s", Integer.toBinaryString(huruf1)).replace(' ','0');
+		System.out.println("Kode ASCII dari "+ huruf1 + " = " + kodehuruf);
+		System.out.println("Kode BINER dari "+ huruf1 + " = " + biner1);
+		
+		// Menggabungkan char menjadi string
+		String kata = "" + huruf1 + huruf2 + angka + simbol;
+		System.out.println("Gabungan char menjadi string: " + kata);
+		
+		
+		
+
+	}
+
+}
